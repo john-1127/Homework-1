6adbd2391dbb66c0cce7f801e7f1aa810ac4c6d2
John Wu <azxenon3@gmail.com>
2024-03-21 18:11:32 +0800

    finish

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    uint256 private registerCallCount = 0;
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        registerCallCount++;
+        if (registerCallCount % 2 == 1) {
+            return 1001;
+        } else {
+            return 123;
+        }
     }
 }
 
@@ -18,6 +23,13 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        IClassroomV2 class = IClassroomV2(msg.sender);
+        if (class.isEnrolled()) {
+            return 123;
+        }
+        else {
+            return 1001;
+        }
     }
 }
 
@@ -25,5 +37,11 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        uint256 balance = gasleft();
+        if (balance > 6630) {
+            return balance;
+        }
+        else
+            return 123;
     }
 }
